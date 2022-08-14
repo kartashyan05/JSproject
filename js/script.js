35e1f10d3ce073baee5b57d9760b4e103308e0e1
Davit <qartashyandav280419@gmail.com>
2022-08-14 12:25:52 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,339 @@
+/* const root = document.querySelector(':root'),
+   rootStyles = getComputedStyle(root);
+
+document.querySelector('button').addEventListener('click', () => {
+   root.style.setProperty('--text-color', 'red');
+}); */
+
+"use strict";
+
+/* let number = 5;
+const LeftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+   a: 50
+};
+
+console.log(ubj);
+
+console.log(name);
+var name = 'Davit'; */
+
+
+/* {
+   let result = 50;
+} */
+
+//console.log(result);
+
+
+// snake_case
+//UPPER_SNAKE_CASE
+//kebab-case
+//PascalCase
+
+//const COLOR_RED = '#F00';
+
+//const _apiBase =
+
+//let number =  4;
+//let Snumber = 4.6;
+
+/* console.log(4 / 0);
+console.log(-5 / 0);
+console.log('string' * 9); */
+
+//const person = 'Alex';
+
+//const bool = true;
+//const bool = false;
+
+//console.log(something);
+
+/* let und;
+console.log(und); */
+
+/* const object = {
+   name: "John",
+   age:25,
+   isMarried: false
+}; */
+
+//console.log(object.name);
+
+//console.log(object["name"]);
+
+/* let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]); */
+
+//const arr = ['a', 'b', 'c']; //Масив
+
+/* arr[10] = '3645';
+console.log(arr);
+const arrObj = {
+   0: 'a',
+   1: 'b',
+   2: 'c',
+   abc: {
+      dg: [{}, {}],
+      def: {
+
+      }
+   }
+};
+const b = 'b'; */
+
+
+//arrObj.b = '1234';
+/* arrObj[b] = '1234';
+console.log(arrObj['b']);
+console.log(arrObj.b); */
+//console.log(arr[1]);
+
+//const omg = {a: 1, b: 2}; //Обьект
+/* const omg = {
+   'Anna': 500,
+   'Alice': 800
+};  */
+
+/* const StoreName = 'My love';
+const storeDescription = {
+   budget: 100000,
+   employees: ['Vache', 'Hrant', 'Edo'],
+   products: {
+      'butter':500,
+      'meat': 1000
+   },
+   open: true
+}; */
+
+//alert('hELLO');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "18");
+//console.log(answer + 5);
+
+/* const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null)); */
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Davit";
+
+alert(`Привет, ${user}`);
+
+console.log('arr' + ' - object');
+console.log(4 + +' - object');
+console.log(4 + +'5');
+
+let incr = 10,
+   decr = 10;
+
+//incr++;
+//decr--;
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 * 4 == 8);
+console.log(2 * 4 == '8');
+console.log(2 * 4 === '8');
+console.log(2 * 4 === 8);
+console.log(2 + 2 * 2 === 8);
+console.log(2 + 2 * 2 != 8);
+console.log(2 + 2 * 2 != '6');
+console.log(2 + 2 * 2 !== '6');
+
+//&& //Оператор И
+//|| //Оператор ИЛИ
+
+const isCheked = false,
+   //isClose = true;
+   isClose = false;
+
+
+//console.log(isCheked && isClose);
+//console.log(isCheked || isClose);
+console.log(isCheked || !isClose);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
